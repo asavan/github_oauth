e1d422ba58bb3576f60d2e4081e955f9ae5159bb
a.savanovich <asavanster@gmail.com>
2023-10-08 06:31:01 +0300

    linter

--- main.js
+++ main.js
@@ -14,19 +14,21 @@ function main() {
 
     const queryString = window.location.search;
     const urlParams = new URLSearchParams(queryString);
-    const code = urlParams.get('code');
+    const code = urlParams.get("code");
     if (!code) {
-        const params = new URLSearchParams({
-              client_id: cid,
-              state: secret_state
-        });
-        const scope = urlParams.get('scope');
+        const pre_params = {
+            client_id: cid,
+            state: secret_state
+        };
+        const scope = urlParams.get("scope");
         if (scope) {
-            params.scope = scope;
+            pre_params.scope = scope;
         }
+
+        const params = new URLSearchParams(pre_params);
         window.location.href = "https://github.com/login/oauth/authorize?" + params.toString();
     } else {
-        const state = urlParams.get('state');
+        const state = urlParams.get("state");
         if (state !== secret_state) {
             console.log("Bad state");
             return false;
@@ -35,15 +37,16 @@ function main() {
         const tokenParams = {
             code: code,
             state: secret_state
-        }
+        };
         const params = new URLSearchParams(tokenParams);
         const fullUrl = tokenUrl + "?" + params.toString();
         console.log(fullUrl);
         fetch(fullUrl).then(response => response.json())
-        .then(data => {
-            addGreet(data);
-            window.history.replaceState({}, document.title, "./");
-        });
+            .then(data => {
+                console.log(data);
+                addGreet(data);
+                window.history.replaceState({}, document.title, "./");
+            });
     }
 }
 main();
